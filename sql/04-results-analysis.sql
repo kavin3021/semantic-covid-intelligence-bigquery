d4d9b51c09b8aca2c327635a63a5ee5233a1bed5
Kavindhiran C <kavindhiran97@gmail.com>
2025-09-01 16:32:01 +0530

    Update 04-results-analysis.sql

--- sql/04-results-analysis.sql
+++ sql/04-results-analysis.sql
@@ -1,1 +1,9 @@
-
+SELECT
+  unique_id,
+  cosine_similarity,
+  ai_summary
+FROM
+  `intellidoc-hackathon-2025.intellidoc_dataset.ai_covid_summaries_from_search`
+ORDER BY
+  cosine_similarity DESC
+LIMIT 20;
